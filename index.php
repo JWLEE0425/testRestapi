03da79ef66c7dddb584e40ba83af4edcd5097f82
JWLEE0425 <43987455+JWLEE0425@users.noreply.github.com>
2018-11-27 13:59:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,21 @@ $app->get('/test', function($request, $response, $args){
     return $response->withStatus(200)->write('Hello world!');
 });
 
+$app->get('/product', function($request, $response, $args){
+    $myArray = array();
+    $con = db_con();
+    $sql = 'select * from product';
+    $result = mysqli_query($con, $sql);
+    
+    while($row = mysqli_fetch_array($result)){
+        $myArray[] = $row;
+    }
+    
+    $response->getbody()->write("JSON : " . json_encode($myArray));
+    return $response;
+});
+
+
 $app->get('/product={name}', function($request, $response, $args){
     $name = $request->getAttribute('name');
     $myArray = array();
