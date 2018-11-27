0b9dc7676f40f34f9a02c68a958b25c8ca085044
JWLEE0425 <43987455+JWLEE0425@users.noreply.github.com>
2018-11-27 11:57:17 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
+include 'config.php';
 require '/usr/share/nginx/html/bin/vendor/autoload.php';
-
 $app = new \Slim\App;
 $app->get('/', function($request, $response, $args) {
     return $response->withStatus(200)->write('Hello worlddd!');
@@ -11,7 +10,7 @@ $app->get('/', function($request, $response, $args) {
 $app->get('/test', function($request, $response, $args){
     return $response->withStatus(200)->write('Hello world!');
 });
-$app->get('/product/{name}', function(Request $request, Response $response){
+$app->get('/product/{name}', function($request, $response){
     $name=$request->getAttribute('name');
     $price = get_price($name);
     if(empty($price)) {
@@ -21,32 +20,28 @@ $app->get('/product/{name}', function(Request $request, Response $response){
     }
     $response->getbody()->write("JSON : ".$json);
     return $response;
-
 });
-
 function get_price($name) {
-    $products = [
-	"book"=>20,
-	"pen"=>10,
-	"pencil"=>5
-    ];
-    foreach($products as $product=>$price) {
-	if($product==$name) {
-	    return $price;
-	    break;
-	}
+    $con = db_con();
+    $sql = 'select * from product';
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($result);
+    
+    foreach($products as $row['name']=>$row['price']) {
+        if($row['name']==$name) {
+            return $row['price'];
+	       break;
+	   }
     }
 }
-
 function response($status, $status_message, $data) {
     header("HTTP/1.1".$status);
-
     $response['status']=$status;
     $response['status message']=$status_message;
     $response['data']=$data;
-
     $json_response = json_encode($response);
     return $json_response;
 }
-
 $app->run();
+
+?>
